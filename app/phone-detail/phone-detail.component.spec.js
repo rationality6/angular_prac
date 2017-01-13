a3300957f764d8c2284a42044fb5cfb56943af53
hyun <rationality6@gmail.com>
2017-01-13 16:43:28 +0900

    save

--- app/phone-detail/phone-detail.component.spec.js
+++ app/phone-detail/phone-detail.component.spec.js
@@ -1,19 +1,19 @@
 'use strict';
 
-describe('phoneDetail', function() {
+describe('phoneDetail', function () {
 
   // Load the module that contains the `phoneDetail` component before each test
   beforeEach(module('phoneDetail'));
 
   // Test the controller
-  describe('PhoneDetailController', function() {
+  describe('PhoneDetailController', function () {
     var $httpBackend, ctrl;
     var xyzPhoneData = {
       name: 'phone xyz',
       images: ['image/url1.png', 'image/url2.png']
     };
 
-    beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
+    beforeEach(inject(function ($componentController, _$httpBackend_, $routeParams) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData);
 
@@ -22,7 +22,7 @@ describe('phoneDetail', function() {
       ctrl = $componentController('phoneDetail');
     }));
 
-    it('should fetch the phone details', function() {
+    it('should fetch the phone details', function () {
       jasmine.addCustomEqualityTester(angular.equals);
 
       expect(ctrl.phone).toEqual({});
@@ -32,5 +32,25 @@ describe('phoneDetail', function() {
     });
 
   });
+
+  it('should simulate promise', inject(function ($q, $rootScope) {
+    var deferred = $q.defer();
+    var promise = deferred.promise;
+    var resolvedValue;
+
+    promise.then(function (value) { resolvedValue = value; });
+    expect(resolvedValue).toBeUndefined();
+
+    // Simulate resolving of promise
+    deferred.resolve(123);
+    // Note that the 'then' function does not get called synchronously.
+    // This is because we want the promise API to always be async, whether or not
+    // it got called synchronously or asynchronously.
+    expect(resolvedValue).toBeUndefined();
+
+    // Propagate promise resolution to 'then' functions using $apply().
+    $rootScope.$apply();
+    expect(resolvedValue).toEqual(123);
+  }));
 
 });
